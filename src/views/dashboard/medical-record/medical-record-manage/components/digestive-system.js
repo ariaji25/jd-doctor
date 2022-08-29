c08343818508b34f8193f453485e658f044363e4
habiburrahman2 <habiburrahman@arvis.id>
2022-08-29 08:38:57 +0800

    [general#40] Slicing ui medical record

---
+++ src/views/dashboard/medical-record/medical-record-manage/components/digestive-system.js
@@ -0,0 +1,61 @@
+import { Box, Flex, Radio, RadioGroup, Stack } from "@chakra-ui/react"
+import { useState } from "react"
+import { useParams, useHistory } from "react-router-dom"
+import colors from "values/colors"
+import { useSnapshot } from 'valtio';
+import stateMedicalRecord from "states/stateMedicalRecord";
+import InputUnderlined from "components/input/InputUnderlined";
+import ButtonMain from "components/button/ButtonMain";
+
+const DigestiveSystem = () => {
+  return (
+    <Stack px={40} py={5}>
+      <Box fontSize={'24px'} fontWeight={'bold'} color={'#505050'} >Sistem pencernaan</Box>
+      <Box fontSize={'18px'} fontWeight={'bold'} color={'#505050'} pl={'20px'} pb={'12px'}>Keadaan mulut</Box>
+      <Flex>
+        <Box flex={1}>Gigi</Box>
+        <Box flex={1}>
+          <RadioGroup>
+            <Flex gap={4} flexWrap={'wrap'}>
+              <Radio>Carries</Radio>
+              <Radio>Tidak</Radio>
+            </Flex>
+          </RadioGroup>
+        </Box>
+      </Flex>
+      <Flex>
+        <Box flex={1}>Memakai gigi palsu</Box>
+        <Box flex={1}>
+          <RadioGroup>
+            <Flex gap={4} flexWrap={'wrap'}>
+              <Radio>Ya</Radio>
+              <Radio>Tidak</Radio>
+            </Flex>
+          </RadioGroup>
+        </Box>
+      </Flex>
+      <Flex>
+        <Box flex={1}>Sakit dada</Box>
+        <Box flex={1}>
+          <RadioGroup>
+            <Flex gap={4} flexWrap={'wrap'}>
+              <Radio>Ya</Radio>
+              <Radio>Tidak</Radio>
+            </Flex>
+          </RadioGroup>
+        </Box>
+      </Flex>
+      <Flex alignItems={'end'}>
+        <Box flex={1}>Lainnya</Box>
+        <Box flex={1}>
+          <InputUnderlined
+            type='text'
+            placeholder={'Isi jika ada'}
+          />
+        </Box>
+      </Flex>
+    </Stack>
+  )
+}
+
+export default DigestiveSystem
---
+++ src/views/dashboard/medical-record/medical-record-manage/components/integumentary-musculoskeletal-system.js
@@ -0,0 +1,73 @@
+import { Box, Flex, Radio, RadioGroup, Stack } from "@chakra-ui/react"
+import { useState } from "react"
+import { useParams, useHistory } from "react-router-dom"
+import colors from "values/colors"
+import { useSnapshot } from 'valtio';
+import stateMedicalRecord from "states/stateMedicalRecord";
+import InputUnderlined from "components/input/InputUnderlined";
+import ButtonMain from "components/button/ButtonMain";
+
+const Integumentary = () => {
+  return (
+    <Stack px={40} py={5}>
+      <Box fontSize={'24px'} fontWeight={'bold'} color={'#505050'} pb={'12px'}>Sistem integumen/muskuloskeletal</Box>
+      <Flex>
+        <Box flex={1}>Turgor kulit</Box>
+        <Box flex={1}>
+          <RadioGroup>
+            <Flex gap={4} flexWrap={'wrap'}>
+              <Radio>Elastis</Radio>
+              <Radio>Sedang</Radio>
+              <Radio>Buruk</Radio>
+            </Flex>
+          </RadioGroup>
+        </Box>
+      </Flex>
+      <Flex>
+        <Box flex={1}>Warna kulit</Box>
+        <Box flex={1}>
+          <RadioGroup>
+            <Flex gap={4} flexWrap={'wrap'}>
+              <Radio>Pucat</Radio>
+              <Radio>Sianosis</Radio>
+              <Radio>Kemerahan</Radio>
+            </Flex>
+          </RadioGroup>
+        </Box>
+      </Flex>
+      <Flex>
+        <Box flex={1}>Kontraktur pada persendian ekstremitas</Box>
+        <Box flex={1}>
+          <RadioGroup>
+            <Flex gap={4} flexWrap={'wrap'}>
+              <Radio>Ya</Radio>
+              <Radio>Tidak</Radio>
+            </Flex>
+          </RadioGroup>
+        </Box>
+      </Flex>
+      <Flex>
+        <Box flex={1}>Kesulitan dalam pergerakan</Box>
+        <Box flex={1}>
+          <RadioGroup>
+            <Flex gap={4}>
+              <Radio>Ya</Radio>
+              <Radio>Tidak</Radio>
+            </Flex>
+          </RadioGroup>
+        </Box>
+      </Flex>
+      <Flex alignItems={'end'}>
+        <Box flex={1}>Lainnya</Box>
+        <Box flex={1}>
+          <InputUnderlined
+            type='text'
+            placeholder={'Isi jika ada'}
+          />
+        </Box>
+      </Flex>
+    </Stack>
+  )
+}
+
+export default Integumentary
